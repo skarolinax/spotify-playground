e1de9253447ea6e7a34353bd997d1c767ece5299
Karolina <k.sech@student.fontys.nl>
2026-05-19 15:38:25 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import "./styles/main.scss";
+
+function App() {
+
+  const [song, setSong] = useState("");
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
